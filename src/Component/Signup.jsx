b8d3340dba76b71b18d05bbb2c9876f93264c8ff
mahiawasthi23 <mahima23@navgurukul.org>
2025-04-04 09:59:05 +0530

    removed local host and add hosting link

--- src/Component/Signup.jsx
+++ src/Component/Signup.jsx
@@ -12,7 +12,7 @@ function Signup() {
   const handleSignup = async (e) => {
     e.preventDefault();
     try {
-      await axios.post("http://localhost:4000/api/auth/signup", { username, email, password });
+      await axios.post("https://gemini-backend-e3b4.vercel.app/api/auth/signup", { username, email, password });
       alert("Signup successful! Please login.");
       navigate("/login");
     } catch (error) {
